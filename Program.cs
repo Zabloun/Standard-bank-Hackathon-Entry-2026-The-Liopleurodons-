e834edae00d6bf17d8cfad7e45b110178bfbfbfb
Emily <emilyckellerman@gmail.com>
2026-05-23 15:08:05 +0200

    Initial commit - created repository on personal laptop with the initial setup for an MVC web application

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace Liopleurodons_Pocket_Business_Helper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
