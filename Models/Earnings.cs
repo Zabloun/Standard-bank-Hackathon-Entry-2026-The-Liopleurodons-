1212ea01bf07ea42dc223af0ef55179dcb6d3f9a
Emily <emilyckellerman@gmail.com>
2026-05-23 21:28:55 +0200

    Added the neccessary code to the Program.cs file

---
+++ Models/Earnings.cs
@@ -0,0 +1,11 @@
+﻿namespace Liopleurodons_Pocket_Business_Helper.Models
+{
+    public class Earnings
+    {
+        public int EarningsId { get; set; }
+
+
+        public decimal TotalEarnings { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Liopleurodons_Pocket_Business_Helper.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,44 @@ namespace Liopleurodons_Pocket_Business_Helper
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+            //Database Option 1: SQL Server
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            //Database Option 2: SQLite 
+            //builder.Services.AddDbContext<AppDbContext>(options =>
+            //    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            // The UseAuthentication and UseAuthorization middleware are added
+            // to the request pipeline to allow use of Identity features such as user authentication and authorization in the application.
+            // This goes hanf in hand with the IdentityUser and IdentityRole services that were added to the service container earlier in the code.
+            app.UseAuthentication();
+            app.UseAuthorization();//order is important
+
+            // least specific route - 0 required segments 
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+
+            SeedData.EnsurePopulated(app);
 
             app.Run();
         }
